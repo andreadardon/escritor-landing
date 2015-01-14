f9d9d430936bd50222cecbb24e539a51ec76e783
Michael Pell <MichaelCPell@gmail.com>
2015-01-13 21:44:58 -0600

    First commit and README update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EscritorLanding::Application.config.secret_key_base = 'd672e44ab5ae78f67082c49efe99186ca410a84d399a4df09c4a37437edca1e40da6d8fa9e203fe42faf467dd1738b6f4072cdd6585ba17ed4709bc3c07f4a0f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EscritorLanding::Application.config.session_store :cookie_store, key: '_escritor-landing_session'
